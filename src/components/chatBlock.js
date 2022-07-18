5fb80ab3dd6c80a64a913bfa53210a1654666016
duquelf90 <duquekun@gmail.com>
2022-07-18 17:02:05 -0500

    update cta

--- src/components/chatBlock.js
+++ src/components/chatBlock.js
@@ -1,13 +1,10 @@
 import Image from "next/image";
-import { Button } from "react-scroll";
 import analytics from "utility/analytics";
 
 
 
 const ChatBlock = () => {
-
   const doWhatsapp = () => {
-    console.log('whasatttt')
     analytics.track('buttonClicked', {
       foo: 'whatsapp'
     }, () => {
@@ -20,6 +17,8 @@ const ChatBlock = () => {
       className="fixed z-[9999] right-2 bottom-2"
       href="https://api.whatsapp.com/send?phone=+525524857429&text=Hola%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20un%20avalúo"
       onClick={doWhatsapp}
+      target="_blank"
+      rel="noreferrer"
     >
       <Image src="/img/whatsapp.png" width={50} height={50} alt="whatsapp" />
     </a>
