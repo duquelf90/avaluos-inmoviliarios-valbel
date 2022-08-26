4607450b88ee6c3c14daedd60c902ab7f8d28116
duquelf90 <duquekun@gmail.com>
2022-08-26 15:01:38 -0500

    before analityc

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 import analytics from "utility/analytics";
 import { ThemeProvider } from "@material-tailwind/react";
 import Script from "next/script";
-import * as gtag from 'utility/gtag'
-
+import * as gtag from "utility/gtag";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -15,13 +14,24 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZR2VYE7BSX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZR2VYE7BSX');
+</script> */}
+
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
           __html: `
         window.dataLayer = window.dataLayer || [];
