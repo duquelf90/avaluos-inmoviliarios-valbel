33688a0f2e7e169a625181d9e692d199a1077ddd
duquelf90 <duquekun@gmail.com>
2022-08-31 14:20:14 -0500

    last change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,29 +2,14 @@ import "styles/globals.css";
 import { useEffect } from "react";
 import analytics from "utility/analytics";
 import { ThemeProvider } from "@material-tailwind/react";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     analytics.page();    
-    // this will fire the Page Track function on every new router change.
   }, []);
 
   return (
-    <>
-      {/* <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
-        `}
-      </Script> */}
+    <>      
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
