794a04918fb5b0078992cc832648e3d9c10f3dda
duquelf90 <duquekun@gmail.com>
2025-03-25 17:04:51 -0600

    update tag manager

--- src/utility/analytics.js
+++ src/utility/analytics.js
@@ -2,18 +2,15 @@ import Analytics from "analytics";
 import googleTagManager from "@analytics/google-tag-manager";
 import googleAnalytics from "@analytics/google-analytics";
 
-const GTM_CONTAINER_ID = "GTM-5JMXK6D";
-const ANALYTICS_ID = "G-ZR2VYE7BSX"
-
 const analytics = Analytics({
   app: "avaluos-web",
   plugins: [
     googleTagManager({
-      containerId: GTM_CONTAINER_ID,
+      containerId: process.env.GTM_CONTAINER_ID,
       enabled: true,
     }),
     googleAnalytics({
-      measurementIds: ANALYTICS_ID,
+      measurementIds: process.env.MEASUREMENT_ID,
     }),
   ],
 });
