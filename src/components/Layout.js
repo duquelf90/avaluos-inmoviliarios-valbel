60183ed0f5639112e68f92be919b3e85e28845ed
duquelf90 <duquekun@gmail.com>
2025-03-25 17:50:12 -0600

    tag test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,15 +4,15 @@ import { GoogleTagManager } from '@next/third-parties/google'
 const Layout = ({ props, children }) => {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-PWJH53XL" />
+      {/* <GoogleTagManager gtmId="GTM-PWJH53XL" /> */}
       <body>
         <div className="antialiased bg-body text-body font-body">
           <div className="container mx-auto">{children}</div>
           <Footer />
         </div>
       </body>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWJH53XL"
-        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+      {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWJH53XL"
+        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript> */}
     </html>
 
   );
