230f6bf4bddb1b51b29884f0c4f782e6db9ae68a
duquelf90 <duquekun@gmail.com>
2025-03-25 17:34:15 -0600

    update again tag manager

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,20 @@
 import Footer from "./Footer";
-import { GoogleTagManager } from '@next/third-parties/google' 
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const Layout = ({ props, children }) => {
   return (
-    <>
-    <GoogleTagManager gtmId="GTM-PWJH53XL" />
-    <div className="antialiased bg-body text-body font-body">
-      <div className="container mx-auto">{children}</div>
-      <Footer/>
-    </div>
-    </>
-    
+    <html lang="en">
+      <GoogleTagManager gtmId="GTM-PWJH53XL" />
+      <body>
+        <div className="antialiased bg-body text-body font-body">
+          <div className="container mx-auto">{children}</div>
+          <Footer />
+        </div>
+      </body>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWJH53XL"
+        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+    </html>
+
   );
 };
 
