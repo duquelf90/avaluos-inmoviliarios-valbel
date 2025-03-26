7b52f10250d304343c894299c26286add264dc9c
duquelf90 <duquekun@gmail.com>
2025-03-25 19:13:48 -0600

    update again

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,14 +5,14 @@ const Layout = ({ props, children }) => {
   return (
     <html lang="en">
       <GoogleTagManager gtmId="GTM-PWJH53XL" />
-      
+      <GoogleAnalytics gaId="G-PZLB8HJXYN" />
       <body>
         <div className="antialiased bg-body text-body font-body">
           <div className="container mx-auto">{children}</div>
           <Footer />
         </div>
       </body> 
-      <GoogleAnalytics gaId="G-PZLB8HJXYN" />      
+            
     </html>
 
   );
